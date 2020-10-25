d3b3293d44c297fb15b0edf6a723fb06ab712f78
Herbert Shin <herbs@Herberts-MacBook-Air-3.local>
2020-10-25 09:27:00 -0400

    Initial prototype of reminder app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button,PushNotification} from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+
+Notifications.scheduleNotificationAsync({
+  content: {
+    title: "Notification",
+    body: 'Change sides!',
+  },
+  trigger: {
+    seconds: 10,
+  },
+});
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={
+        {fontSize: 50, 
+        flex: 1,
+        paddingTop:50,}}>
+          The Remind App
+          </Text>
+        <TextInput style={styles.textbox}>Enter Reminder</TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +33,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFECEE',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textbox:{
+    backgroundColor:'#BDBDBD',
+    flex:4,
+    marginHorizontal:20,
+    marginVertical: 10,
+  },
+  button:{
+    flex:3,
+  }
 });
+
